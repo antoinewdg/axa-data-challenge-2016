adb94a2ce2b2d4ad4a004e48d2fbf13411488071
AntoineWDG <antoinewendlinger@gmail.com>
2016-02-26 15:40:12 +0100

    Incremental featurization

--- featurizer.py
+++ featurizer.py
@@ -1,63 +1,88 @@
 import pandas as pd
 from tqdm import tqdm, trange
+import tables
 
 
-def load_raw_data(filename):
-    print("Reading data from file")
+class Featurizer:
+    def __init__(self):
+        self.assignments = set()
 
-    dtype = {
-        'DATE': object,
-        'WEEK_END': int,
-        'DAY_WE_DS': str,
-        'ASS_ASSIGNMENT': str,
-        'CSPL_RECEIVED_CALLS': int
-    }
+    def featurize(self, in_filename, out_filename, chunksize=10 ** 6):
 
-    cols = ['DATE', 'WEEK_END', 'DAY_WE_DS', 'ASS_ASSIGNMENT', 'CSPL_RECEIVED_CALLS']
-    return pd.read_csv(filename, sep=";", usecols=cols, dtype=dtype, parse_dates=['DATE'])
+        self._learn_structure(in_filename, chunksize)
+        dtype = {
+            'DATE': object,
+            'WEEK_END': int,
+            'DAY_WE_DS': str,
+            'ASS_ASSIGNMENT': str,
+            'CSPL_RECEIVED_CALLS': int
+        }
 
+        cols = ['DATE', 'WEEK_END', 'DAY_WE_DS', 'ASS_ASSIGNMENT', 'CSPL_RECEIVED_CALLS']
+        chunks = pd.read_csv(in_filename, sep=";", usecols=cols, dtype=dtype, parse_dates=['DATE'], chunksize=chunksize)
 
-def featurize(df):
-    features = pd.DataFrame()
-    _featurize_day_of_the_week(df, features)
-    _featurize_time_slot(df, features)
-    _featurize_assignment(df, features)
-    _featurize_number_of_calls(df, features)
+        with tables.open_file(out_filename, mode='w') as out:
+            atom = tables.Float64Atom()
+            i = 0
+            arr = None
+            for chunk in chunks:
+                features = self._featurize_chunk(chunk)
+                if i == 0:
+                    arr = out.create_earray(out.root, 'features', atom=atom, shape=(0, features.shape[1]))
+                arr.append(features.as_matrix())
+                i += 1
+            out.close()
 
-    return features
+    def _learn_structure(self, filename, chunksize=10 ** 6):
+        self.assignments = set()
 
+        dtype = {'ASS_ASSIGNMENT': str}
+        cols = ['ASS_ASSIGNMENT']
+        chunks = pd.read_csv(filename, sep=";", usecols=cols, dtype=dtype, chunksize=chunksize)
 
-def _featurize_day_of_the_week(df, features):
-    print("Featurizing days of the week")
+        for df in tqdm(chunks):
+            self.assignments.update(df.ASS_ASSIGNMENT.unique())
 
-    days = [('monday', 'Lundi'), ('tuesday', 'Mardi'), ('wednesday', 'Mercredi'), ('thursday', 'Jeudi'),
-            ('friday', 'Vendredi'), ('saturday', 'Samedi'), ('sunday', 'Dimanche')]
+        print(self.assignments)
 
-    features['is_week_end'] = df.WEEK_END
-    for i in trange(7):
-        en, fr = days[i]
-        features[en] = (df.DAY_WE_DS == fr).astype(int)
+    def _featurize_chunk(self, df):
+        features = pd.DataFrame()
+        self._featurize_day_of_the_week(df, features)
+        self._featurize_time_slot(df, features)
+        self._featurize_assignment(df, features)
+        self._featurize_number_of_calls(df, features)
 
-    print()
+        return features
 
+    def _featurize_day_of_the_week(self, df, features):
+        print("Featurizing days of the week")
 
-def _featurize_time_slot(df, features):
-    print("Featurizing time slots")
+        days = [('monday', 'Lundi'), ('tuesday', 'Mardi'), ('wednesday', 'Mercredi'), ('thursday', 'Jeudi'),
+                ('friday', 'Vendredi'), ('saturday', 'Samedi'), ('sunday', 'Dimanche')]
 
-    for h in trange(24):
-        for s in range(2):
-            features['time_slot_' + str(2 * h + s)] = ((df.DATE.dt.hour == h) & (df.DATE.dt.minute == 30 * s)) \
-                .astype(int)
+        features['is_week_end'] = df.WEEK_END
+        for i in trange(7):
+            en, fr = days[i]
+            features[en] = (df.DAY_WE_DS == fr).astype(int)
 
-    print()
+        print()
 
+    def _featurize_time_slot(self, df, features):
+        print("Featurizing time slots")
 
-def _featurize_assignment(df, features):
-    print("Featurizing assignment")
-    assignments = df.ASS_ASSIGNMENT.unique()
-    for i in range(len(assignments)):
-        features['assignment_' + str(i)] = (df.ASS_ASSIGNMENT == assignments[i]).astype(int)
+        for h in trange(24):
+            for s in range(2):
+                features['time_slot_' + str(2 * h + s)] = ((df.DATE.dt.hour == h) & (df.DATE.dt.minute == 30 * s)) \
+                    .astype(int)
 
+        print()
 
-def _featurize_number_of_calls(df, features):
-    features['n_calls'] = df.CSPL_RECEIVED_CALLS
+    def _featurize_assignment(self, df, features):
+        print("Featurizing assignment")
+        i = 0
+        for assignment in self.assignments:
+            features['assignment_' + str(i)] = (df.ASS_ASSIGNMENT == assignment).astype(int)
+            i += 1
+
+    def _featurize_number_of_calls(self, df, features):
+        features['n_calls'] = df.CSPL_RECEIVED_CALLS
