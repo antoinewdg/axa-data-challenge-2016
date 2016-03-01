66d570a5ec12c37a36b3c6819963027630a492d7
AntoineWDG <antoinewendlinger@gmail.com>
2016-03-01 16:08:58 +0100

    Composite regressor

---
+++ compare_regressors.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+from classifier_search import best_classifier
+from composite_regressor import CompositeRegressor
+from featurize_training_set import load_featurized_training_set
+
+from sklearn.linear_model import SGDRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.cross_validation import KFold
+
+
+def compare_classifiers(filename):
+    clf_simple = SGDRegressor()
+    clf_composite = CompositeRegressor(best_classifier(), SGDRegressor())
+
+    features = load_featurized_training_set(filename)
+    X = features.drop(['DATE', 'n_calls'], axis=1).as_matrix().astype(float)
+    y = features.n_calls.as_matrix()
+
+    mse_compsite = []
+    mse_simple = []
+
+    classifiers = {"simple": clf_simple, "composite": clf_composite}
+    mse = {"simple": [], "composite": []}
+
+    kf = KFold(len(X), n_folds=5, shuffle=True)
+    for train, test in kf:
+        for type in classifiers:
+            clf = classifiers[type]
+            clf.fit(X[train], y[train])
+            predicted = clf.predict(X[test])
+            mse[type].append(mean_squared_error(y[test], predicted))
+
+    print("Average for simple classifier: " + str(np.mean(mse['simple'])))
+    print("Average for composite classifier: " + str(np.mean(mse['composite'])))
+
+
+if __name__ == "__main__":
+    compare_classifiers("files/train_featurized.pkl")
