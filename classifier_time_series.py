23b63eaf743289c7f4945524ede6a6bd14b982e5
Felipe Vieira Frujeri <ffrujeri@gmail.com>
2016-03-03 13:19:09 +0100

    Correcting date format

--- classifier_time_series.py
+++ classifier_time_series.py
@@ -8,11 +8,12 @@ from sklearn.metrics import mean_squared_error
 from featurizer import *
 from pandas.tseries.offsets import *
 import math
+import matplotlib.pyplot as plt
 
 training_df = load_training_set("files/train_groupedby.csv")
 training_df = training_df.set_index('DATE', drop=False)
 
-submission_df = load_submission("files/submission_test.txt")
+submission_df = load_submission("files/submission_2.txt")
 submission_df = submission_df.set_index('DATE', drop=False)
 
 y_true = submission_df.as_matrix()[:, -1]
@@ -28,15 +29,23 @@ for i in trange(0, submission_df.shape[0]):
 	local_df = local_df[local_df.DAY_WE_DS == day]
 	local_df = local_df[local_df.index.time == time]
 
-	y_pred[i] = local_df['CSPL_RECEIVED_CALLS'].mean()
+	# y_pred[i] = local_df['CSPL_RECEIVED_CALLS'].mean()
+	y_pred[i] = pd.rolling_mean(local_df['CSPL_RECEIVED_CALLS'], window=3, min_periods=1)[1]
 	# print y_pred[i]
 
-y_pred_round = [int(math.ceil(x)) if x > 0 else 0 for x in y_pred]
+y_pred_round = y_pred
+
+# y_pred_round = [int(math.round(x)) if x > 0 else 0 for x in y_pred]
 # print(y_pred_round)
 
+x = range(len(y_pred))
+plt.plot(x, y_pred, x, y_true)
 submission_df.prediction = y_pred_round
+submission_df = submission_df.set_index('DATE', drop=False)
+submission_df['DATE'] = submission_df.index.strftime('%Y-%m-%d %H:%M:%S.000')
 print submission_df
-submission_df[['DATE', 'ASS_ASSIGNMENT', 'prediction']].to_csv('results/submission_test00.txt', sep='\t', index=False)
+
+submission_df[['DATE', 'ASS_ASSIGNMENT', 'prediction']].to_csv('results/submission_test02.txt', sep='\t', index=False)
 
 print('MSE round: '),
 print(mean_squared_error(y_true, y_pred_round))
